9535ac4ca8710c573f382c0efd260d60540f7d8d
Devoloperr <mirkamilezizli085@gmail.com>
2024-06-20 18:21:35 +0400

    Homework 18

---
+++ 1app.js
@@ -0,0 +1,2 @@
+let a = prompt("Enter a 4-digit number.");
+console.log(Number(a[0]) + Number(a[1]) + Number(a[2]) + Number(a[3]));
---
+++ app.js
@@ -0,0 +1,3 @@
+let fullName = prompt("Enter your surname and name");
+let word = fullName.split(" ");
+console.log("Salam, " + word[1] + "!");
